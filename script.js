f68bfbc635de3e8e3bb179561f4fc4cbb68dd454
Vishesh-Kumar <19vishesh@gmail.com>
2023-01-03 02:13:06 +0530

    correct the formatting, no changes

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ window.onscroll = () => {
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
 
-  if(window.scrollY > 60){
-    document.querySelector('#scroll-top').classList.add('active');
-  }else{
-    document.querySelector('#scroll-top').classList.remove('active');
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
   }
 };
 
-function loader(){
-  document.querySelector('.loader-container').classList.add('fade-out');
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
 }
 
-function fadeOut(){
+function fadeOut() {
   setInterval(loader, 3000);
 }
 
